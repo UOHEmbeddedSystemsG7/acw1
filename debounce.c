e06fdbc1fd6de8ef4d8adba117903a1c7122cb09
Ash Entwisle <ash.entwisle@protonmail.com>
2025-11-21 10:55:26 +0000

    two candidates
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- debounce.c
+++ debounce.c
@@ -1,129 +1,79 @@
 #include <builtins.h>
 #include "debounce.h"
 
-uint8_t btn_dec() {
-  static unsigned char last = 0;
-  static unsigned char stable = 0;
-  static unsigned char count = 0;
+typedef struct {
+  unsigned char last;
+  unsigned char stable;
+  unsigned char count;
+} debounce_state_t;
 
-  unsigned char raw = PORTCbits.RC7 ? 1u : 0u;
-
-  if (raw == stable) { 
-    if (count < 5u)  count++;
-  }
+uint8_t debounce(unsigned char raw, debounce_state_t *state) {
+  if (raw == state->stable) { 
+      if (state->count < 5u) state->count++;
+  } 
   
   else { 
-    count = 0; 
-    stable = 0;
+    state->count = 0; 
+    state->stable = raw;
   }
 
-  if (count >= 5u) {
-    if (stable && !last) {
-      last = 1;
+  if (state->count >= 5u) {
+      
+    if (state->stable && !state->last) {
+      state->last = 1;
       return 1;
     }
-    if (!stable) last = 0;
+    
+    if (!state->stable) state->last = 0;
   }
+
+  return 0;
+}
+
+
+inline uint8_t db2(unsigned char raw, debounce_state_t state) {
+  if (raw == state.stable) { 
+      if (state.count < 5u) state.count++;
+  } 
+  
+  else { 
+    state.count = 0; 
+    state.stable = raw;
+  }
+
+  if (state.count >= 5u) {
+      
+    if (state.stable && !state.last) {
+      state.last = 1;
+      return 1;
+    }
+    
+    if (!state.stable) state.last = 0;
+  }
+
+  return 0;
+}
+
+uint8_t btn_dec() {
+  static debounce_state_t state = {0, 0, 0};
+  unsigned char raw = PORTCbits.RC7 ? 1u : 0u;
+  return db2(raw, state);
 }
 
 uint8_t btn_sel() {
-  static unsigned char last = 0;
-  static unsigned char stable = 0;
-  static unsigned char count = 0;
-
+  static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC1 ? 1u : 0u;
-
-  if (raw == stable) { 
-    if (count < 5u)  count++;
-  }
-  
-  else { 
-    count = 0; 
-    stable = 0;
-  }
-
-  if (count >= 5u) {
-    if (stable && !last) {
-      last = 1;
-      return 1;
-    }
-    if (!stable) last = 0;
-  }
+  return debounce(raw, &state);
 }
 
 uint8_t button_increment() {
-  static unsigned char last = 0;
-  static unsigned char stable = 0;
-  static unsigned char count = 0;
-
+  static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC7 ? 1u : 0u;
-
-  if (raw == stable) { 
-    if (count < 5u)  count++;
-  }
-  
-  else { 
-    count = 0; 
-    stable = 0;
-  }
-
-  if (count >= 5u) {
-    if (stable && !last) {
-      last = 1;
-      return 1;
-    }
-    if (!stable) last = 0;
-  }
+  return debounce(raw, &state);
 }
 
 uint8_t button_cyc() {
-  static unsigned char last = 0;
-  static unsigned char stable = 0;
-  static unsigned char count = 0;
-
+  static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC0 ? 1u : 0u;
-
-  if (raw == stable) { 
-    if (count < 5u)  count++;
-  }
-  
-  else { 
-    count = 0; 
-    stable = 0;
-  }
-
-  if (count >= 5u) {
-    if (stable && !last) {
-      last = 1;
-      return 1;
-    }
-    if (!stable) last = 0;
-  }
+  return debounce(raw, &state);
 }
-
-// TODO: figure out how to do this so we dont have to copy stuff about
-//uint8_t debounce(uint8_t bit) {
-//  static unsigned char last = 0;
-//  static unsigned char stable = 0;
-//  static unsigned char count = 0;
-//
-//  unsigned char raw = bit ? 1u : 0u;
-//
-//  if (raw == stable) { 
-//    if (count < 5u)  count++;
-//  }
-//  
-//  else { 
-//    count = 0; 
-//    stable = 0;
-//  }
-//
-//  if (count >= 5u) {
-//    if (stable && !last) {
-//      last = 1;
-//      return 1;
-//    }
-//    if (!stable) last = 0;
-//  }
-//
-//}
