5a69f4662b248a2cc63c6b18a231d351dae96488
Ash Entwisle <ash.entwisle@protonmail.com>
2025-11-21 10:56:46 +0000

    went with pre-process bc pointers
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- debounce.c
+++ debounce.c
@@ -7,31 +7,8 @@ typedef struct {
   unsigned char count;
 } debounce_state_t;
 
-uint8_t debounce(unsigned char raw, debounce_state_t *state) {
-  if (raw == state->stable) { 
-      if (state->count < 5u) state->count++;
-  } 
-  
-  else { 
-    state->count = 0; 
-    state->stable = raw;
-  }
 
-  if (state->count >= 5u) {
-      
-    if (state->stable && !state->last) {
-      state->last = 1;
-      return 1;
-    }
-    
-    if (!state->stable) state->last = 0;
-  }
-
-  return 0;
-}
-
-
-inline uint8_t db2(unsigned char raw, debounce_state_t state) {
+inline uint8_t debounce(unsigned char raw, debounce_state_t state) {
   if (raw == state.stable) { 
       if (state.count < 5u) state.count++;
   } 
@@ -57,23 +34,23 @@ inline uint8_t db2(unsigned char raw, debounce_state_t state) {
 uint8_t btn_dec() {
   static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC7 ? 1u : 0u;
-  return db2(raw, state);
+  return debounce(raw, state);
 }
 
 uint8_t btn_sel() {
   static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC1 ? 1u : 0u;
-  return debounce(raw, &state);
+  return debounce(raw, state);
 }
 
 uint8_t button_increment() {
   static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC7 ? 1u : 0u;
-  return debounce(raw, &state);
+  return debounce(raw, state);
 }
 
 uint8_t button_cyc() {
   static debounce_state_t state = {0, 0, 0};
   unsigned char raw = PORTCbits.RC0 ? 1u : 0u;
-  return debounce(raw, &state);
+  return debounce(raw, state);
 }
