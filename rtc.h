e02e8dc91cfd15daef7023adce0f11a47670b525
HLowe03 <H.LOWE-2021@hull.ac.uk>
2025-11-24 14:58:30 +0000

    rtc: Add inital code

---
+++ rtc.h
@@ -0,0 +1,32 @@
+#ifndef MCC
+#include "mcc_generated_files/system/system.h"
+#define MCC
+#endif 
+
+
+#ifndef RTC_H
+#define	RTC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+typedef struct { uint8_t second, minute, hour;} rtc_time_t;
+    
+static uint8_t dec_to_bcd(uint8_t dec);
+static uint8_t bcd_to_dec(uint8_t bcd);
+
+void rtc_write_reg(uint8_t addr, uint8_t data);
+void rtc_read_reg(uint8_t addr, uint8_t* buf, uint8_t buf_len);
+
+void rtc_init();
+void rtc_set_time(rtc_time_t* time);
+void rtc_get_time(rtc_time_t* time);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif
