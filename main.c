a99953540bd850760cee0cf5d4e80f815a3973a6
HLowe03 <H.LOWE-2021@hull.ac.uk>
2025-12-01 13:34:46 +0000

    main: Add EEPROM test code

--- main.c
+++ main.c
@@ -9,19 +9,26 @@
 #include "adc.h"
 #include "iic.h"
 #include "rtc.h"
+#include "eeprom.h"
 
 
 //#define XII_TEMP
 uint16_t celsius = 0;
 
-#define XII_TIME
+//#define XII_TIME
 rtc_time_t now;
 uint64_t ms = 0;
 
-#if defined(XII_TEMP) && defined(XII_TIME)
+//#define XII_COUNT
+uint8_t counter = 0;
+
+
+#if (defined(XII_TEMP) && defined(XII_TIME))   || \
+    (defined(XII_TEMP) && defined(XII_COUNT)) || \
+    (defined(XII_TIME) && defined(XII_COUNT))
+    // More than one is defined
 #error "Only define XII_TEMP or XII_TIME, not both"
 #endif
-
 
 
 
@@ -37,15 +44,43 @@ int main(void)
     rtc_init();
     
     xiiseg_init();
-
+    
+#ifdef XII_COUNT
+    counter = eeprom_read_byte(EEPROM_ADDR_COUNTER);
+    TRISC = 0xff;
+    ANSELC = 0;
+#endif
+    
     while(1)
     {
         // we want this on ISR prob
         xiiseg_multiplex();
+        
+        
+        
+        
+        
+    #ifdef XII_COUNT
+        if (PORTCbits.RC0 == 0) {
+            counter++;
+            __delay_ms(10);
+        }
+        if (PORTCbits.RC1 == 0) {
+            counter--;
+            __delay_ms(10);
+        }
+        xiiseg_display(3, digits[counter % 10u]);
+        xiiseg_display(2, digits[(counter / 10) % 10]);
+        xiiseg_display(1, digits[(counter / 100) % 10]);
+        
+        ms++;
+        if (ms > 500) { // approx every 3 seconds
+            ms=0;
+            eeprom_write_byte(EEPROM_ADDR_COUNTER, counter);
+            
+        }
+    #endif
 
-        
-        
-        
     #ifdef XII_TEMP
         celsius = adc_to_celsius(read_adc());
         
@@ -71,6 +106,4 @@ int main(void)
     #endif
 
     }
-    
-    
 }
