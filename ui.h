8239db1f508e6fe7686d4b719fe2bc6a038df899
HLowe03 <H.LOWE-2021@hull.ac.uk>
2025-12-08 13:32:00 +0000

    ui.x: Add rendering methods

--- ui.h
+++ ui.h
@@ -4,6 +4,7 @@
 #define UI_H
 
 #include "debounce.h"
+#include "rtc.h"
 #include <stdint.h>
 #ifndef MCC
 #include "mcc_generated_files/system/system.h"
@@ -23,6 +24,23 @@ uint8_t btn_cyc();
 uint8_t btn_sel();
 uint8_t btn_inc();
 uint8_t btn_dec();
+
+uint16_t ui_temp_upper;
+uint16_t ui_temp_lower;
+uint8_t ui_alarm_sel;
+
+rtc_time_t ui_temp_start;
+rtc_time_t ui_temp_end;
+rtc_time_t ui_alarm_time;
+
+char ui_empty_line[16] = "                ";
+
+void ui_render_temp_upper(uint8_t just_entered);
+void ui_render_temp_lower(uint8_t just_entered);
+void ui_render_heating_start(uint8_t just_entered);
+void ui_render_heating_end(uint8_t just_entered);
+void ui_render_alarm_time(uint8_t just_entered);
+void ui_render_alarm_tone(uint8_t just_entered);
 
 
 #ifdef __cplusplus
