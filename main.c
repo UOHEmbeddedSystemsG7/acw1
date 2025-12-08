cea410ec2a3f81d0fcf643a288e4adb041696704
HLowe03 <H.LOWE-2021@hull.ac.uk>
2025-12-08 13:34:58 +0000

    main.c: Add menu

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define MCC
 #endif 
 
+#include <string.h>
 
 #include "sevenseg.h"
 #include "adc.h"
@@ -28,15 +29,10 @@ uint32_t lcd_last_run_count = 0;
 uint32_t rtc_last_fetch_count = 0;
 
 // lcd
-uint8_t lcd_counter = 0;
-
-#if (defined(XII_TEMP) && defined(XII_TIME))   || \
-    (defined(XII_TEMP) && defined(XII_COUNT)) || \
-    (defined(XII_TIME) && defined(XII_COUNT))
-    // More than one is defined
-#error "Only define XII_TEMP or XII_TIME, not both"
-#endif
-
+uint8_t ui_selected_screen = 0;
+#define TOP_SCREEN 6
+uint8_t counter = 0;
+uint8_t screen_swapped = 1;
 
 int main(void)
 {
@@ -48,9 +44,53 @@ int main(void)
     rtc_init();
     xiiseg_init();
     lcd_init();
-
+    
+    // button init?
+    TRISC |= 0xC3;
+    ANSELC &= ~(0xC3);
+    
     while(1)
     {
+        
+        // ------------------- USER INPUTS ------------------- 
+        // ------------------- USER INPUTS ------------------- 
+        // ------------------- USER INPUTS ------------------- 
+        
+        // one goes up a menu, other goes back
+        if (PORTCbits.RC1 == 0) {
+            ui_selected_screen++;
+            if (ui_selected_screen > TOP_SCREEN) {
+                ui_selected_screen = 0;
+            }
+            screen_swapped = 1;
+            __delay_ms(10);
+        }
+        if (PORTCbits.RC0 == 0) {
+            
+            if (ui_selected_screen == 0) {
+                ui_selected_screen = TOP_SCREEN;
+            } else {
+                ui_selected_screen--;
+            }
+            screen_swapped = 1;
+            __delay_ms(10);
+        }
+        
+        if (PORTCbits.RC7 == 0) {
+            ui_increment(ui_selected_screen);
+            __delay_ms(10);
+        }
+        if (PORTCbits.RC6 == 0) {
+            ui_decrement(ui_selected_screen);
+            __delay_ms(10);
+        }
+        
+        
+        // -------------------- DISPLAY AND RENDERING --------------------
+        // -------------------- DISPLAY AND RENDERING --------------------
+        // -------------------- DISPLAY AND RENDERING --------------------
+        
+        // XII SEG
         // we want this on ISR prob
         xiiseg_multiplex();
         
@@ -60,30 +100,71 @@ int main(void)
         xiiseg_display(2, digits[celsius % 10u]);
         xiiseg_display(1, (digits[(celsius / 10) % 10] + 0x80) ); // adding 0x80 turns on RD7 which is the dp
         xiiseg_display(0, digits[(celsius / 100) % 10]);
-        
-        
-        
-        // LCDTIME/DATE
+
+        // LCD
         // run this every ~100 loops
         if (main_loop_count - lcd_last_run_count >= 100){
-            lcd_write_date(1,9, &date_now, &date_before);
-            lcd_write_time(2,9, &time_now, &time_before);
+            if (screen_swapped) {
+               // clean bottom line, top done in render
+
+               lcd_write_string(2,1, ui_empty_line);
+               
+               // set both to 0 so they refresh
+               memset(&date_before, 0, sizeof(date_before)); 
+               memset(&time_before, 0, sizeof(time_before));
+            }
             
-            date_before = date_now;
-            time_before = time_now;
+            switch (ui_selected_screen) {
+                case 0: // render the default time and date.
+                    lcd_write_date(1,9, &date_now, &date_before);
+                    lcd_write_time(2,9, &time_now, &time_before);
+            
+                    date_before = date_now;
+                    time_before = time_now;
+                    break;
+                case 1:
+                    ui_render_temp_upper(screen_swapped);
+                    break;
+                case 2:
+                    ui_render_temp_lower(screen_swapped);
+                    break;
+                case 3:
+                    ui_render_heating_start(screen_swapped);
+                    break;
+                case 4:
+                    ui_render_heating_end(screen_swapped);
+                    break;
+                case 5:
+                    ui_render_alarm_time(screen_swapped);
+                    break;
+                case 6:
+                    ui_render_alarm_tone(screen_swapped);
+                    break;
+           }
+            screen_swapped = 0;
             
         }
-       
+        
         
         // RTC Refresh
         // run every ~500
-        if (main_loop_count - rtc_last_fetch_count >= 500) {
-            main_loop_count=0l;
+        if (main_loop_count - rtc_last_fetch_count >= 100) {
             rtc_get_time(&time_now);
             rtc_get_date(&date_now); // can be removed if wanted quicker
         }
+
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // MAIN LOOP LOGIC!
         main_loop_count+= 1;
     }
 }
