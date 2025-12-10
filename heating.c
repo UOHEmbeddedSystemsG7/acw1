d5f60b0c4702b7cd96d900c2b0f5c6c4286b5d56
OSCAR JEFFRIES <O.JEFFRIES-2021@hull.ac.uk>
2025-12-10 19:58:13 +0000

    heating: added ability to disable window

--- heating.c
+++ heating.c
@@ -57,41 +57,42 @@ void heating_logic(uint16_t celsius, rtc_time_t time_now){
     uint8_t start_hour = ui_temp_start.hour;
     uint8_t start_min = ui_temp_start.minute;
     
-    // checks hour then minute for whether heating allowed to turn on
-    if ((time_now.hour >= start_hour) && (time_now.hour <= end_hour)){
-        
-        if ((time_now.hour == end_hour) && (time_now.minute > end_min)){
-            heating_off();
-        } else if ((time_now.hour == start_hour) && (time_now.minute < start_min)){
-            heating_off();
-        } else {
-            // if temp goes lower than bottom threshold then heating turns on
-            if (celsius <= ui_temp_lower){
-                heating_on();
-                isHeated = 0;
-            } else if ((celsius > ui_temp_lower) && (celsius < ui_temp_upper) && (isHeated == 0)){ // as heating approaches upper threshold LEDs turn off
-                
-                if(celsius < ui_temp_upper - 40){
-                    LATA |= (0x0f<<4);
-                } else if((celsius >= ui_temp_upper -40) && (celsius < ui_temp_upper -30)){
-                    LATA &= ~(0x01<<4);
-                    LATA |= (0x07<<5);
-                } else if ((celsius >= ui_temp_upper -30) && (celsius < ui_temp_upper -20)){
-                    LATA &= ~(0x03<<4);
-                    LATA |= (0x03<<6);
-                } else if (celsius < ui_temp_upper -10){
-                    LATA &= ~(0x07<<4);
-                    LATA |= (0x01<<7);
+    if (ui_window_disabled = 0){
+        // checks hour then minute for whether heating allowed to turn on
+        if ((time_now.hour >= start_hour) && (time_now.hour <= end_hour)){
+
+            if ((time_now.hour == end_hour) && (time_now.minute > end_min)){
+                heating_off();
+            } else if ((time_now.hour == start_hour) && (time_now.minute < start_min)){
+                heating_off();
+            } else {
+                // if temp goes lower than bottom threshold then heating turns on
+                if (celsius <= ui_temp_lower){
+                    heating_on();
+                    isHeated = 0;
+                } else if ((celsius > ui_temp_lower) && (celsius < ui_temp_upper) && (isHeated == 0)){ // as heating approaches upper threshold LEDs turn off
+
+                    if(celsius < ui_temp_upper - 40){
+                        LATA |= (0x0f<<4);
+                    } else if((celsius >= ui_temp_upper -40) && (celsius < ui_temp_upper -30)){
+                        LATA &= ~(0x01<<4);
+                        LATA |= (0x07<<5);
+                    } else if ((celsius >= ui_temp_upper -30) && (celsius < ui_temp_upper -20)){
+                        LATA &= ~(0x03<<4);
+                        LATA |= (0x03<<6);
+                    } else if (celsius < ui_temp_upper -10){
+                        LATA &= ~(0x07<<4);
+                        LATA |= (0x01<<7);
+                    }
+                }
+
+                // if temp in room greater than heating_upper the LED will be off regardless of time
+                if(celsius >= ui_temp_upper){
+                    isHeated = 1;
+                    heating_off();
                 }
             }
-          
-            // if temp in room greater than heating_upper the LED will be off regardless of time
-            if(celsius >= ui_temp_upper){
-                isHeated = 1;
-                heating_off();
-            }
-        }
-    } else {heating_off();}   
-
+        } else {heating_off();}
+    } else {heating_off();}
 }
 
